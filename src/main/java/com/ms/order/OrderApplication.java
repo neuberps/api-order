6b8889c70d7158db639a806198a6500e05a62309
Fabio Argona <128233610+Fabio-Argona@users.noreply.github.com>
2024-03-16 12:36:32 -0300

    Update OrderApplication.java

--- src/main/java/com/ms/order/OrderApplication.java
+++ src/main/java/com/ms/order/OrderApplication.java
@@ -2,12 +2,14 @@ package com.ms.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"com.ms.order.repository"})
 @SpringBootApplication
 public class OrderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrderApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
 
 }
